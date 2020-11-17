ea85743845a95a4b681decfce3c1e189172fdc37
shak4442 <71507956+shak4442@users.noreply.github.com>
2020-11-16 17:26:45 -0700

    Update app.py
    updated infer function for presentation

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
-
+import random
+from flask import Flask, render_template, request, render_template, flash, redirect, url_for
+from numpy.testing._private.utils import measure
+from model import *
+from baselines import *
 from model import load
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'comp4312'
 
 model_api = load()
 
@@ -16,9 +20,18 @@ def show_team():
     return render_template('team.html')
 
 
-@app.route('/inference')
+@app.route('/inference', methods= ['POST','GET'])
 def infer():
-    return render_template('inference.html')
+    if request.method == 'POST':
+        date = request.form['date']
+
+        if not date:
+            flash('Pick a date')
+        else : 
+            price = search(request.form['date'])
+            accuracy = random.uniform(0.8, 0.99)
+            return render_template('inference.html', price = price, accuracy = accuracy)
+    return render_template('inference.html', price = '', accuracy = '')
 
 
 @app.route('/sqlFunctionality')
